114bd73b448ffb46ee4ab0c028d80a253cfeb1a9
joseAB <jose.albelt1817@gmail.com>
2025-06-23 18:34:32 -0500

    skills pages

--- src/app/pages/skills-pages/skills-pages.component.ts
+++ src/app/pages/skills-pages/skills-pages.component.ts
@@ -6,4 +6,40 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './skills-pages.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SkillsPagesComponent { }
+export class SkillsPagesComponent { 
+  frontend = [
+  { title: 'HTML', logo: 'html' },
+  { title: 'CSS', logo: 'css' },
+  { title: 'JavaScript', logo: 'javascript' },
+  { title: 'TypeScript', logo: 'typescript' },
+  { title: 'React', logo: 'react' },
+  { title: 'Bootstrap', logo: 'bootstrap' },
+  { title: 'TailwindCSS', logo: 'tailwindcss' },
+  { title: 'PrimeNG', logo: 'primeng' },
+  { title: 'DaisyUI', logo: 'daisyui-icon' },
+  { title: 'Chart.js', logo: 'chartjs' },
+  { title: 'RxJS', logo: 'rxjs' }
+];
+
+  backend = [
+    { title: 'Supabase', logo: 'supabase' },
+    { title: 'API', logo: 'api' }
+  ];
+  
+  tools = [
+    { title: 'Git', logo: 'git' },
+    { title: 'GitHub', logo: 'github' },
+    { title: 'Azure DevOps', logo: 'azuredevops' },
+    { title: 'Jira', logo: 'jira' },
+    { title: 'Trello', logo: 'trello' },
+    { title: 'Postman', logo: 'postman' },
+    { title: 'Slack', logo: 'slack' },
+    { title: 'Microsoft Teams', logo: 'microsoftteams' }
+  ];
+  
+  nexts = [
+    { title: 'Node.js', logo: 'nodejs' },
+    { title: 'Vercel', logo: 'vercel' },
+    { title: 'Docker', logo: 'docker' },
+  ]
+}

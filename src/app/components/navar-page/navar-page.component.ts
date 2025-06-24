35ae7ec4f08561adcd8ddfb34f1e6f57240db8d5
joseAB <jose.albelt1817@gmail.com>
2025-06-23 23:52:12 -0500

    traductor insalacion del i18n

--- src/app/components/navar-page/navar-page.component.ts
+++ src/app/components/navar-page/navar-page.component.ts
@@ -1,10 +1,30 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navar-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
   templateUrl: './navar-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavarPageComponent { }
+export class NavarPageComponent {
+  isEnglish = false;
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('es');
+  }
+
+  onToggle(event: Event) {
+    this.isEnglish = (event.target as HTMLInputElement).checked;
+    const lang = this.isEnglish ? 'en' : 'es';
+    this.changeLang(lang);
+  }
+
+  changeLang(lang: string) {
+    console.log(lang)
+    this.translate.use(lang);
+  }
+}
